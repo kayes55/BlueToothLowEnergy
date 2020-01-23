dc7208f7fe409cf6496e8d93ad24394b427bb547
kayes55 <kayes.shamil@gmail.com>
2020-01-23 11:09:14 +0600

    Project Created

---
+++ BlueToothLowEnergy/BlueToothLowEnergy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueToothLowEnergy
+//
+//  Created by Miftah Uddin on 1/23/20.
+//  Copyright © 2020 Miftah Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
